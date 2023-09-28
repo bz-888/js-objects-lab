01eb1f0b8f4241db3e97666a9a3a838dbc843c4c
bz-888 <benjamin.zheng95@gmail.com>
2023-09-28 11:12:31 -0500

    objects lesson

---
+++ js/app.js
@@ -0,0 +1,91 @@
+console.log("App.js is loading.");
+
+// defining an object literal
+// key value pairs <- want in your language
+
+// keys = name, color, isReal, etc.
+// values = "Big Foot", "brown", true, etc.
+const bigFoot = {
+    name: "Big Foot",
+    color: "brown",
+    isReal: true,
+    height: 7, // feet
+    diet: "vegetarian",
+    woodlandFriends: [
+        {name: "bunny", age: 4},
+        {name: "squirrel", age: 5}],
+    eat (plant) {
+        console.log(`Lets eat ${plant}`);
+    },
+    hide (location) {
+        console.log(`Lets hide in the ${location}`)
+    },
+    run () {
+
+    }
+};
+
+bigFoot.eat("tree");
+bigFoot.eat("lettuce");
+bigFoot.eat("kale");
+
+// add an argument to the hide function called location
+// when you call it, it should say:
+// Lets hide in "location"
+
+bigFoot.hide("bush");
+bigFoot.hide("shadows");
+
+
+// can use the delete operator to delete a property
+delete bigFoot.isReal
+
+// log the age of bunny and the name of squirrel
+console.log(bigFoot.woodlandFriends[0].age);
+console.log(bigFoot.woodlandFriends[1].name);
+
+
+// I want to see if bigFoot is real
+// retrieve the value of the isReak key (property) in bigFoot
+console.log(bigFoot.isReal);
+
+// gettomg a property (value) ^^^
+
+// see what diet bigFoot has''
+console.log(bigFoot.diet);
+
+
+// set key value pairs
+
+// Change the color of bigFoot
+bigFoot.color = "strawberry blonde";
+
+// Change the diet of bigFoot
+bigFoot.diet = "Omnivore";
+console.log(bigFoot.diet);
+
+// Print out deet from woodlandFriends
+console.log(bigFoot.woodlandFriends[2]);
+
+// For each woodlandFriend, use sayHello function
+// You should see three logs
+// Hello, bunny
+// Hello, squirrel
+// Hello, deer
+
+function sayHello (name) {
+    return `Hello, ${name}`;
+};
+
+bigFoot.woodlandFriends.forEach(function (friend) {
+    console.log(sayHello(friend));
+});
+
+
+// to access values
+// dot syntax
+bigFoot.name; // "Big Foot"
+// bracket syntax
+bigFoot["name"]; // "Big Foot"
+
+bigFoot["woodlandFriends"][0]; // "bunny"

fde2846b7d06156c24beac8ea2a04e30f78d6be1
bz-888 <benjamin.zheng95@gmail.com>
2023-09-28 11:17:26 -0500

    commented out lesson

--- js/app.js
+++ js/app.js
@@ -1,91 +1,94 @@
-console.log("App.js is loading.");
+// console.log("App.js is loading.");
 
-// defining an object literal
-// key value pairs <- want in your language
+// // defining an object literal
+// // key value pairs <- want in your language
 
-// keys = name, color, isReal, etc.
-// values = "Big Foot", "brown", true, etc.
-const bigFoot = {
-    name: "Big Foot",
-    color: "brown",
-    isReal: true,
-    height: 7, // feet
-    diet: "vegetarian",
-    woodlandFriends: [
-        {name: "bunny", age: 4},
-        {name: "squirrel", age: 5}],
-    eat (plant) {
-        console.log(`Lets eat ${plant}`);
-    },
-    hide (location) {
-        console.log(`Lets hide in the ${location}`)
-    },
-    run () {
+// // keys = name, color, isReal, etc.
+// // values = "Big Foot", "brown", true, etc.
+// const bigFoot = {
+//     name: "Big Foot",
+//     color: "brown",
+//     isReal: true,
+//     height: 7, // feet
+//     diet: "vegetarian",
+//     woodlandFriends: [
+//         {name: "bunny", age: 4},
+//         {name: "squirrel", age: 5}],
+//     eat (plant) {
+//         console.log(`Lets eat ${plant}`);
+//     },
+//     hide (location) {
+//         console.log(`Lets hide in the ${location}`)
+//     },
+//     run () {
 
-    }
-};
+//     }
+// };
 
-bigFoot.eat("tree");
-bigFoot.eat("lettuce");
-bigFoot.eat("kale");
+// bigFoot.eat("tree");
+// bigFoot.eat("lettuce");
+// bigFoot.eat("kale");
 
-// add an argument to the hide function called location
-// when you call it, it should say:
-// Lets hide in "location"
+// // add an argument to the hide function called location
+// // when you call it, it should say:
+// // Lets hide in "location"
 
-bigFoot.hide("bush");
-bigFoot.hide("shadows");
-
-
-// can use the delete operator to delete a property
-delete bigFoot.isReal
-
-// log the age of bunny and the name of squirrel
-console.log(bigFoot.woodlandFriends[0].age);
-console.log(bigFoot.woodlandFriends[1].name);
+// bigFoot.hide("bush");
+// bigFoot.hide("shadows");
 
 
-// I want to see if bigFoot is real
-// retrieve the value of the isReak key (property) in bigFoot
-console.log(bigFoot.isReal);
+// // can use the delete operator to delete a property
+// delete bigFoot.isReal
 
-// gettomg a property (value) ^^^
-
-// see what diet bigFoot has''
-console.log(bigFoot.diet);
+// // log the age of bunny and the name of squirrel
+// console.log(bigFoot.woodlandFriends[0].age);
+// console.log(bigFoot.woodlandFriends[1].name);
 
 
-// set key value pairs
+// // I want to see if bigFoot is real
+// // retrieve the value of the isReak key (property) in bigFoot
+// console.log(bigFoot.isReal);
 
-// Change the color of bigFoot
-bigFoot.color = "strawberry blonde";
+// // gettomg a property (value) ^^^
 
-// Change the diet of bigFoot
-bigFoot.diet = "Omnivore";
-console.log(bigFoot.diet);
-
-// Print out deet from woodlandFriends
-console.log(bigFoot.woodlandFriends[2]);
-
-// For each woodlandFriend, use sayHello function
-// You should see three logs
-// Hello, bunny
-// Hello, squirrel
-// Hello, deer
-
-function sayHello (name) {
-    return `Hello, ${name}`;
-};
-
-bigFoot.woodlandFriends.forEach(function (friend) {
-    console.log(sayHello(friend));
-});
+// // see what diet bigFoot has''
+// console.log(bigFoot.diet);
 
 
-// to access values
-// dot syntax
-bigFoot.name; // "Big Foot"
-// bracket syntax
-bigFoot["name"]; // "Big Foot"
+// // set key value pairs
 
-bigFoot["woodlandFriends"][0]; // "bunny"
+// // Change the color of bigFoot
+// bigFoot.color = "strawberry blonde";
+
+// // Change the diet of bigFoot
+// bigFoot.diet = "Omnivore";
+// console.log(bigFoot.diet);
+
+// // Print out deet from woodlandFriends
+// console.log(bigFoot.woodlandFriends[2]);
+
+// // For each woodlandFriend, use sayHello function
+// // You should see three logs
+// // Hello, bunny
+// // Hello, squirrel
+// // Hello, deer
+
+// function sayHello (name) {
+//     return `Hello, ${name}`;
+// };
+
+// bigFoot.woodlandFriends.forEach(function (friend) {
+//     console.log(sayHello(friend));
+// });
+
+
+// // to access values
+// // dot syntax
+// bigFoot.name; // "Big Foot"
+// // bracket syntax
+// bigFoot["name"]; // "Big Foot"
+
+// bigFoot["woodlandFriends"][0]; // "bunny"
+
+
+
